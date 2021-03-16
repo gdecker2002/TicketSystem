43ab307837d190106dd7dd4307597c7770b4f8b7
Gage <gdecker@dmacc.edu>
2021-03-15 19:27:21 -0500

    Add project files.

---
+++ TicketSystem/Models/ToDo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace TicketSystem.Models
+{
+    public class ToDo
+    {
+        public int TicketID { get; set; }
+
+        [Required(ErrorMessage = "Please enter Name.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter Description.")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Please enter Description.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter Name.")]
+        public string Name { get; set; }
+    }
+}
